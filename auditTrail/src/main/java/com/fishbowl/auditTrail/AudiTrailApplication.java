342512aaa231574011702203d41e32c14675b3ce
rkumar3ic <rkumar_ic@fishbowl.com>
2019-09-17 16:23:11 +0530

    deleted few files

--- auditTrail/src/main/java/com/fishbowl/auditTrail/AudiTrailApplication.java
+++
@@ -1,8 +0,0 @@
-package com.fishbowl.auditTrail;
-
-public class AudiTrailApplication {
-
-	public static void main(String[] args) {
-	}
-
-}
--- auditTrail/src/main/java/com/fishbowl/auditTrail/dao/AuditTrailDAO.java
+++
@@ -1,8 +0,0 @@
-package com.fishbowl.auditTrail.dao;
-
-public interface AuditTrailDAO<T> {
-	
-	public T insertAuditTrailDetails(T model);
-	public int updateAuditTrailDetails(T model);
-
-}
